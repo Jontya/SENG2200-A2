8c61eea7fa699d106bd36caeb030dfb483ddd8f1
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-04-18 16:48:44 +1000

    ComesBefore Method Functionality

--- PlanarShape.java
+++ PlanarShape.java
@@ -1,5 +1,4 @@
 public abstract class PlanarShape implements Comparable<PlanarShape>{
-    Point points[];
 
     public PlanarShape(){
 
@@ -11,8 +10,20 @@ public abstract class PlanarShape implements Comparable<PlanarShape>{
 
     public abstract double originDistance();
 
+    public abstract void initShape(double[] values);
+
     public int compareTo(PlanarShape shape){
-        return 0;
+        
+        double numerator = Math.abs(this.area() - shape.area());
+        double denominator = (this.area() + shape.area() / 2);
+        double percentDifference = (numerator / denominator) * 100;
+        if(percentDifference > 0.1 && this.area() < shape.area()){
+            return 1;
+        }
+        else if(percentDifference <= 0.1){
+            return 1;
+        }
+        return -1;
     }
     
 }
