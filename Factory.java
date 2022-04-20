0fae91b58ac00cd89605d5f7ce42a154028ff0ce
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-04-21 01:59:28 +1000

    Comments

--- Factory.java
+++ Factory.java
@@ -1,3 +1,14 @@
+//---------------------------------------------------------------------------------------------------
+/** SENG2200 A2
+*** Jonty Atkinson (C3391110)
+*** 21/04/22
+***
+*** Factory:
+*** Defines the Factory interface and the shapeFactory method
+**/
+//---------------------------------------------------------------------------------------------------
+
 public interface Factory {
+    
     PlanarShape shapeFactory(char shapeToken, double[] shapePoints) throws Exception;
 }
