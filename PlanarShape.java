fd83189c73595d38cf2d0d2d39e8616f84eed60d
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-04-21 00:08:57 +1000

    Updated CompareTo Method
    List will now out put the sorted list in descending order (previously ascending)

--- PlanarShape.java
+++ PlanarShape.java
@@ -1,4 +1,5 @@
 public abstract class PlanarShape implements Comparable<PlanarShape>{
+    private Point[] points;
 
     public PlanarShape(){
 
@@ -16,15 +17,15 @@ public abstract class PlanarShape implements Comparable<PlanarShape>{
         double numerator = Math.abs(shape.area() - this.area());
         double denominator = (shape.area() + this.area() / 2);
         double percentDifference = (numerator / denominator) * 100;
-        if(percentDifference > 0.1 && this.area() < shape.area()){
-            return 1;
+        if(percentDifference > 0.05 && this.area() < shape.area()){
+            return -1;
         }
-        else if(percentDifference <= 0.1){
+        else if(percentDifference <= 0.05){
             if(this.originDistance() < shape.originDistance()){
-                return 1;
+                return -1;
             }
         }
-        return -1;
+        return 1;
     }
     
 }
