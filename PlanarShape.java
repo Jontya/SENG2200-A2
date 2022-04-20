f7459ad22e9cd009719beeada58b53dec08390f7
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-04-20 16:29:30 +1000

    Small Changes To CompareTo()

--- PlanarShape.java
+++ PlanarShape.java
@@ -4,24 +4,25 @@ public abstract class PlanarShape implements Comparable<PlanarShape>{
 
     }
 
+    @Override
     public abstract String toString();
 
     public abstract double area();
 
     public abstract double originDistance();
 
-    public abstract void initShape(double[] values);
-
+    @Override
     public int compareTo(PlanarShape shape){
-        
-        double numerator = Math.abs(this.area() - shape.area());
-        double denominator = (this.area() + shape.area() / 2);
+        double numerator = Math.abs(shape.area() - this.area());
+        double denominator = (shape.area() + this.area() / 2);
         double percentDifference = (numerator / denominator) * 100;
         if(percentDifference > 0.1 && this.area() < shape.area()){
             return 1;
         }
         else if(percentDifference <= 0.1){
-            return 1;
+            if(this.originDistance() < shape.originDistance()){
+                return 1;
+            }
         }
         return -1;
     }
